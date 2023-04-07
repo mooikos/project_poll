c754c25427fe34393b709fe1398c4259c9829933
Massimiliano De Vivo <massimiliano.devivo>
2023-04-07 13:36:46 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# debugger
+gem "pry-byebug"
+
+# codestyle
+gem "rubocop"
+
+# application framework + server
+gem "sinatra"
+gem "webrick"
---
+++ test_heroku.rb
@@ -0,0 +1,12 @@
+# test_heroku.rb
+require 'sinatra/base'
+
+class TestHeroku < Sinatra::Base
+
+  get "*" do
+    [404, "Sorry page not found!!"]
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
